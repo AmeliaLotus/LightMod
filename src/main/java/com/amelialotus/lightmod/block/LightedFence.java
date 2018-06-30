b52c9d0f6df18f98a2dee755d506d1e549c610df
Lotus <AmeliaLotus@users.noreply.github.com>
2018-06-29 19:15:26 -0700

    Added lighted ladders, and code for non-connected fences!

---
+++ src/main/java/com/amelialotus/lightmod/block/LightedFence.java
@@ -0,0 +1,35 @@
+package com.amelialotus.lightmod.block;
+
+import com.amelialotus.lightmod.LightMod;
+import com.amelialotus.lightmod.init.BlockInit;
+import com.amelialotus.lightmod.init.ItemInit;
+import com.amelialotus.lightmod.util.IHasModel;
+
+import net.minecraft.block.BlockFence;
+import net.minecraft.block.material.MapColor;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+
+public class LightedFence extends BlockFence implements IHasModel
+{
+
+	public LightedFence(Material materialIn, MapColor mapColorIn) {
+		super(materialIn, mapColorIn);
+		String name = "lighted_fence";
+		setUnlocalizedName(name);
+		setRegistryName(name);
+		setCreativeTab(LightMod.tabLightMod);
+		setLightLevel(1.0F);
+		this.setResistance(2000.0F);
+		
+		BlockInit.BLOCKS.add(this);
+		ItemInit.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+		
+	}
+
+	@Override
+	public void registerModels() {
+		LightMod.proxy.registerItemRenderer(Item.getItemFromBlock(this), 0, "inventory");		
+	}	
+}
